8e1cea2b9133c17f93f7d2a7ac501f44ad248d74
Guilherme Cesconeto <cesconeto@gmail.com>
2022-05-24 10:05:08 -0300

    feat: part 1 done

--- tutosite/tutosite/wsgi.py
+++ tutosite/tutosite/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tutosite.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "tutosite.settings")
 
 application = get_wsgi_application()
