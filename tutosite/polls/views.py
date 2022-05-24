fb3497e717b9392610c55980f6953cb03e8ca706
gcesconeto <78055417+gcesconeto@users.noreply.github.com>
2022-05-24 16:25:58 -0300

    Adds generic views refactor and tests (#1)
    * feat: implement generic views
    
    * feat: initial tests implemented
    
    * feat: part 5 done

--- tutosite/polls/views.py
+++ tutosite/polls/views.py
@@ -1,33 +1,56 @@
 from django.urls import reverse
 from django.http import HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
+from django.views import generic
+from django.utils import timezone
 from .models import Question, Choice
 
 
-def index(request):
-    question_list = Question.objects.order_by("-pub_date")[:5]
-    context = {"question_list": question_list}
-    return render(request, "polls/index.html", context)
+class IndexView(generic.ListView):
+    template_name = "polls/index.html"
+    context_object_name = "question_list"
+
+    def get_queryset(self):
+        """
+        Return the last five published questions (not including those set to be
+        published in the future).
+        """
+        return Question.objects.filter(pub_date__lte=timezone.now()).order_by(
+            "-pub_date"
+        )[:5]
 
 
-def detail(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, "polls/detail.html", {"question": question})
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = "polls/detail.html"
+
+    def get_queryset(self):
+        """
+        Excludes any questions that aren't published yet.
+        """
+        return Question.objects.filter(pub_date__lte=timezone.now())
 
 
-def results(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, "polls/results.html", {"question": question})
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = "polls/results.html"
+
+    def get_queryset(self):
+        """
+        Excludes any questions that aren't published yet.
+        """
+        return Question.objects.filter(pub_date__lte=timezone.now())
 
 
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
     try:
-        sel_choice = question.choice_set.get(pk=request.POST['choice'])
+        sel_choice = question.choice_set.get(pk=request.POST["choice"])
     except (KeyError, Choice.DoesNotExist):
-        return render(request, "polls/detail.html", {
-            "question": question,
-            "error_msg": "You didnt select a choice"},
+        return render(
+            request,
+            "polls/detail.html",
+            {"question": question, "error_msg": "You didnt select a choice"},
         )
     else:
         sel_choice.votes += 1
